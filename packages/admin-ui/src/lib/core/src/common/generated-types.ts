6f6029a55321d37c2bbfaa8d9f96ea5d04e22793
Ghazan Khan <ghazankhan27@hotmail.com>
2025-01-23 16:04:29 +0500

    chore: Types added

--- packages/admin-ui/src/lib/core/src/common/generated-types.ts
+++ packages/admin-ui/src/lib/core/src/common/generated-types.ts
@@ -2056,6 +2056,7 @@ export enum HistoryEntryType {
   CUSTOMER_PASSWORD_RESET_VERIFIED = 'CUSTOMER_PASSWORD_RESET_VERIFIED',
   CUSTOMER_PASSWORD_UPDATED = 'CUSTOMER_PASSWORD_UPDATED',
   CUSTOMER_REGISTERED = 'CUSTOMER_REGISTERED',
+  CUSTOMER_REJECTED = 'CUSTOMER_REJECTED',
   CUSTOMER_REMOVED_FROM_GROUP = 'CUSTOMER_REMOVED_FROM_GROUP',
   CUSTOMER_VERIFIED = 'CUSTOMER_VERIFIED',
   ORDER_CANCELLATION = 'ORDER_CANCELLATION',
@@ -2950,6 +2951,8 @@ export type Mutation = {
   moveCollection: Collection;
   refundOrder: RefundOrderResult;
   reindex: Job;
+  /** Reject a customer */
+  rejectCustomer: Customer;
   /** Removes Collections from the specified Channel */
   removeCollectionsFromChannel: Array<Collection>;
   /** Removes the given coupon code from the draft Order */
@@ -3619,6 +3622,12 @@ export type MutationMoveCollectionArgs = {
 
 export type MutationRefundOrderArgs = {
   input: RefundOrderInput;
+};
+
+
+export type MutationRejectCustomerArgs = {
+  id: Scalars['ID']['input'];
+  reason?: InputMaybe<Scalars['String']['input']>;
 };
 
 
