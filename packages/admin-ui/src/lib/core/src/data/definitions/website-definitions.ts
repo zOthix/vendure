e89e8c0d6bca3cd733f65f3c49be2d3f333e756a
Ghazan Khan <ghazankhan27@hotmail.com>
2025-04-16 17:56:18 +0500

    feat: Added definitions for weblinks

--- packages/admin-ui/src/lib/core/src/data/definitions/website-definitions.ts
+++ packages/admin-ui/src/lib/core/src/data/definitions/website-definitions.ts
@@ -1,12 +1,35 @@
 import { gql } from 'apollo-angular';
 
+export const WEBLINK_FRAGMENT = gql`
+    fragment WebLink on WebLink {
+        id
+        link
+        linkText
+        position
+        featuredAsset {
+            id
+            createdAt
+            updatedAt
+            preview
+            focalPoint {
+                x
+                y
+            }
+        }
+    }
+`;
+
 export const WEBSITE_FRAGMENT = gql`
     fragment Website on Website {
         content
         footerContent
         announcementBarText
         id
+        weblinks {
+            ...WebLink
+        }
     }
+    ${WEBLINK_FRAGMENT}
 `;
 
 export const GET_WEBSITE_QUERY = gql`
@@ -25,4 +48,13 @@ export const UPDATE_WEBSITE_MUTATION = gql`
         }
     }
     ${WEBSITE_FRAGMENT}
+`;
+
+export const UPDATE_WEBLINK_MUTATION = gql`
+    mutation UpdateWebLinks($input: UpdateWebLinksInput!) {
+        updateWebLinks(input: $input) {
+            ...WebLink
+        }
+    }
+    ${WEBLINK_FRAGMENT}
 `;
--- packages/admin-ui/src/lib/core/src/data/providers/website-data.service.ts
+++ packages/admin-ui/src/lib/core/src/data/providers/website-data.service.ts
@@ -1,5 +1,9 @@
 import * as Codegen from '../../common/generated-types';
-import { GET_WEBSITE_QUERY, UPDATE_WEBSITE_MUTATION } from '../definitions/website-definitions';
+import {
+    GET_WEBSITE_QUERY,
+    UPDATE_WEBLINK_MUTATION,
+    UPDATE_WEBSITE_MUTATION,
+} from '../definitions/website-definitions';
 
 import { BaseDataService } from './base-data.service';
 
@@ -7,7 +11,9 @@ export class WebsiteDataService {
     constructor(private baseDataService: BaseDataService) {}
 
     getWebsite() {
-        return this.baseDataService.query<Codegen.GetWebsiteQueryVariables>(GET_WEBSITE_QUERY);
+        return this.baseDataService.query<Codegen.GetWebsiteQuery, Codegen.GetWebsiteQueryVariables>(
+            GET_WEBSITE_QUERY,
+        );
     }
 
     updateWebsite(input: Codegen.UpdateWebsiteInput) {
@@ -15,6 +21,15 @@ export class WebsiteDataService {
             Codegen.UpdateWebsiteMutation,
             Codegen.UpdateWebsiteMutationVariables
         >(UPDATE_WEBSITE_MUTATION, {
+            input,
+        });
+    }
+
+    updateWebLinks(input: Codegen.UpdateWebLinksInput) {
+        return this.baseDataService.mutate<
+            Codegen.UpdateWebLinksMutation,
+            Codegen.UpdateWebLinksMutationVariables
+        >(UPDATE_WEBLINK_MUTATION, {
             input,
         });
     }
